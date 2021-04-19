499521d2b733f21d30ba75ec6b342877a8d709ac
Tássio Rego <tassiorego@gmail.com>
2021-04-18 21:59:49 -0300

    feat: improve script for development and production

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const isDevelopment = process.env.NODE_ENV !== 'production';
 
 module.exports = {
   mode: isDevelopment ? 'development': 'production',
-  devtool: 'eval-source-map',
+  devtool: isDevelopment?  'eval-source-map': 'source-map',
   entry: path.resolve(__dirname, 'src', 'index.jsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
